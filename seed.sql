4f2dd90e3bd6cc9690b1da90bf98153c6ac716cb
Ashton Walden <ashtonwalden@icloud.com>
2020-07-11 02:27:49 -0600

    Added dummy data for 'employee_role' table

--- seed.sql
+++ seed.sql
@@ -1,6 +1,6 @@
 -- uses employee management system database
 USE ems_db;
--- Chooses to insert data into 'department' table
+-- Inserts dummy data into 'department' table
 INSERT INTO department (deptName)
 VALUES ("test_1");
 
@@ -15,3 +15,13 @@ VALUES ("test_4");
 
 INSERT INTO department (deptName)
 VALUES ("test_5");
+
+-- Inserts dummy data into 'employee' table
+INSERT INTO employee_role (title, salary)
+VALUES ("test_1_lead", 100000.00);
+
+INSERT INTO employee_role (title, salary)
+VALUES ("test_1_employee", 50000.00);
+
+INSERT INTO employee_role (title, salary)
+VALUES ("test_2_employee", 45000.00);
