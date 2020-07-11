adb24b4b96b523aa879ff83b69c15ec3347f1392
Ashton Walden <ashtonwalden@icloud.com>
2020-07-11 01:41:54 -0600

    tested seed.sql on department table

---
+++ seed.sql
@@ -0,0 +1,17 @@
+-- uses employee management system database
+USE ems_db;
+-- Chooses to insert data into 'department' table
+INSERT INTO department (deptName)
+VALUES ("test_1");
+
+INSERT INTO department (deptName)
+VALUES ("test_2");
+
+INSERT INTO department (deptName)
+VALUES ("test_3");
+
+INSERT INTO department (deptName)
+VALUES ("test_4");
+
+INSERT INTO department (deptName)
+VALUES ("test_5");
