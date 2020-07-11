f63e9b06dc13a30271f6e42c24a95f26e6eb7cf9
Ashton Walden <ashtonwalden@icloud.com>
2020-07-11 03:06:34 -0600

    added dummy data to 'employee' table

--- seed.sql
+++ seed.sql
@@ -2,26 +2,12 @@
 USE ems_db;
 -- Inserts dummy data into 'department' table
 INSERT INTO department (deptName)
-VALUES ("test_1");
+VALUES ("test_1"), ("test_2"), ("test_3");
 
-INSERT INTO department (deptName)
-VALUES ("test_2");
+-- Inserts dummy data into 'employee_role' table
+INSERT INTO employee_role (title, salary, deptID)
+VALUES ("test_1_lead", 100000.00, 1), ("test_1_employee", 50000.00, 2), ("test_2_employee", 45000.00, 3);
 
-INSERT INTO department (deptName)
-VALUES ("test_3");
-
-INSERT INTO department (deptName)
-VALUES ("test_4");
-
-INSERT INTO department (deptName)
-VALUES ("test_5");
-
--- Inserts dummy data into 'employee' table
-INSERT INTO employee_role (title, salary)
-VALUES ("test_1_lead", 100000.00);
-
-INSERT INTO employee_role (title, salary)
-VALUES ("test_1_employee", 50000.00);
-
-INSERT INTO employee_role (title, salary)
-VALUES ("test_2_employee", 45000.00);
+-- Inserts dummy data into 'employee table'
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Test", "Testerson", 1), ("John", "Testerson", 2, 1), ("Bayley", "Testerson", 3, 1);
